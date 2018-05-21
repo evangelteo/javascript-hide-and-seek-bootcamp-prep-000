fc18e0d20e42b051a6c0a57294df0cced6db5452
Evangel Teo <evangelteo@gmail.com>
2018-05-21 19:02:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const ranks = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < ranks.length; i++) {
+    const childr = ranks[i].children
+    for (let j = 0; j < childr.length; j++) {
+      childr[j].innerHTML = parseInt(childr[j].innerHTML)+n
+    }
+  }
+}
+
+function deepestChild() {
+  var grandnode = document.querySelector('div#grand-node')
+  while (grandnode.hasChildNodes()) {
+	if (grandnode.childElementCount === 0) {
+		return grandnode
+    }
+    grandnode = grandnode.children[0]
+  }
+}
